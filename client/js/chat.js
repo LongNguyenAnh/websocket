54244b875830e4eab357780ae907b8f031cc592f
MrLonely <longnguyen_58@yahoo.com>
2012-04-18 10:13:17 +0700

    Update client/js/chat.js

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,12 +1,14 @@
+var token=": ";
 var chatManager = {
 	socket : null,
 	clientID : null,
+	clientName : null,
 	keyPress : function() {
 		var that = this;
 		$(document).keydown(function(event) {
 			if ( event.keyCode === 13 ) {
 				that.sendMsg($("#chatinput").val());
-				$("#chatinput").html("");
+				$("#chatinput").val("");
 			}
 		});
 	},	
@@ -14,14 +16,19 @@ var chatManager = {
 		this.socket = clientManager.getSocket();
 		var payload = {
 			type : "CHAT_MESSAGE",
-			clientID : null,
-			clientName : null,
+			clientID : this.clientID,
+			clientName : $("#myusernameedit").val(),
 			data : text,
 			date : Date.now()
 		};
 		this.socket.send(JSON.stringify(payload));
 	},
-	receiveMsg : function(text) {
-		$("#chattext").append(text+"<br>");
+	receiveMsg : function(msg) {
+		if($("#myusernameedit").val() === "")
+		{
+			alert("Please input a Name");
+		}
+		else
+		{$("#chattext").append("<b>" + msg.clientName + "</b>" + token + msg.data +"<br>");}
 	}
 }
